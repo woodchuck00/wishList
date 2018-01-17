c75f8fd2eaaa7297812a20e7c958874e5e0a95a7
theo <tatheodor@gmail.com>
2018-01-16 22:56:18 -0800

    Update model.php

--- model/model.php
+++ model/model.php
@@ -7,28 +7,28 @@ class Model {
 	protected $user;
 		
 	public function __construct() {  
-		$this->list = new ItemList();
-		$this->user = new User();
+	  $this->list = new ItemList();
+	  $this->user = new User();
 	} 
 	public function login(&$errors) {
-		return $this->user->login($errors);
+	  return $this->user->login($errors);
 	}
 	public function logout() {
-		return $this->user->logout();
+	  return $this->user->logout();
 	}
 	public function register(&$errors) {
-		return $this->user->register($errors);
+	  return $this->user->register($errors);
 	}
 	public function forgot_password(&$errors) {
-		return $this->user->forgot_password($errors);
+	  return $this->user->forgot_password($errors);
 	}
 	public function logged_in() {
-		return $this->user->logged_in();
+	  return $this->user->logged_in();
 	}
-  public function getList() {
-		return $this->list->getList();
+        public function getList() {
+	  return $this->list->getList();
 	}
 	public function addItem(&$errors) {
-		return $this->list->add($errors);
+	  return $this->list->add($errors);
 	}
 }
